55a50c028b778353994a5747a021ff41ff73977a
furkan özcan <gittim.geldim.git@gmail.com>
2023-10-25 16:53:01 -0700

    footer duzen

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='w-full h-10 tex bg-gray-950 text-gray-200 flex'>
-        <p>All rights reserved @jsonserver</p>
+        <p>All rights reserved @ghosthippo</p>
     </div>
   )
 }
